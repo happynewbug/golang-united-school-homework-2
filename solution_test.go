eb8affd076dcfe6d8b7ee194a5b4088542e4735d
Eldar Azimov <chaevnicher@gmail.com>
2022-02-17 18:12:59 +0300

    HW-002: test for method CalcSquare

---
+++ solution_test.go
@@ -0,0 +1,31 @@
+package square
+
+import (
+	"math"
+	"testing"
+)
+
+type param struct {
+	sideLen  float64
+	sidesNum int8
+	expects  float64
+}
+
+func TestCalcSquare(t *testing.T) {
+	params := [6]param{
+		{5, 0, 50 * math.Pi},
+		{6, 3, 18},
+		{7, 4, 49},
+		{8, 15, 0},
+	}
+
+	for _, param := range params {
+		actual := CalcSquare(param.sideLen, param.sidesNum)
+
+		if actual != param.expects {
+			t.Errorf("actual %f expect %f", actual, param.expects)
+		}
+	}
+
+	t.Log("OK!")
+}
